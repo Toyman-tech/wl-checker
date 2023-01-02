d30ba39323c621bbbd6ee4f7c95904e6f9e06b1c
Toyman-tech <adiguno204@gmail.com>
2023-01-02 14:42:49 +0100

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import ./Component/addy.com
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class 
+App extends Component {
+  constructor(){
+    super();
+    this.state = {addy:[],
+    checkAddy:"" };
+    this.handleChange= this.handleChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+handleChange(event){
+  this.setState({checkAddy: event.target.value});
+}
+handleSubmit(event){
+  alert(`is this addy wled? ${this.state.checkAddy}`);
+  event.preventDefault();
 }
 
+render(){
+  const {addy, checkAddy} = this.state;
+  console.log(addy);
+  const wallets = ["habeeb"];
+  //console.log(checkAddy);  
+  const filteredMonsters = addy.filter(monster => monster.name.toLowerCase().includes(addy.toLocaleLowerCase()));
+ 
+
+  return (
+      <div className="App searchbox">
+       <div className='search'>
+       <h1 className='heading'>are you dumplisted?</h1>
+       <form className='faform' onSubmit={this.handleSubmit} >
+        <input  type="text" className="searcharea" 
+         placeholder='wallet address' value={checkAddy} onChange={this.handleChange}
+         />
+         <input className="button"  type="submit" 
+         value="submit" placeholder='wallet address' 
+        />
+         </form>
+       <h2 className='heading'>you are dumplisted!</h2>
+       
+         </div>
+             <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  
+  
+  }
+ 
+
+}
 export default App;
